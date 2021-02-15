9802d61360c1e0a9f44979521f121bf56ccd9375
marcoortolani <marco.ortolani@gmail.com>
2021-02-15 09:30:01 +0000

    Small change to Main.java

--- alice/src/main/java/uk/ac/keele/csc20004/Main.java
+++ alice/src/main/java/uk/ac/keele/csc20004/Main.java
@@ -7,7 +7,8 @@ package uk.ac.keele.csc20004;
 /**
  * Main class to provide an example of use for the concrete implementation of a
  * TextMiner. Here, I show the different results when using an implementation 
- * based on Maps as compared to one based on a List.
+ * based on Maps as compared to one based on a List. 
+ * Add a line
  *
  * @see MapTextMiner
  * @see ListTextMiner
